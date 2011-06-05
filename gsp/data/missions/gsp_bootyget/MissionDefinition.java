63efa0e33ab8c44c5b5b19e2ea967af91169ab3b
trylobot <trylobot@c9139e88-106a-4b82-92ae-96bf13407d3a>
2011-06-05 22:41:49 +0000

    git-svn-id: https://trylobot.unfuddle.com/svn/trylobot_starfarer@53 c9139e88-106a-4b82-92ae-96bf13407d3a

--- gsp/data/missions/gsp_bootyget/MissionDefinition.java
+++ gsp/data/missions/gsp_bootyget/MissionDefinition.java
@@ -34,9 +34,12 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 		api.addToFleet(FleetSide.PLAYER, "gsp_hyou_wing", FleetMemberType.FIGHTER_WING, "Bilge Rat", false);
 		api.addToFleet(FleetSide.PLAYER, "gsp_hyou_wing", FleetMemberType.FIGHTER_WING, "Hogshead", false);
     api.addToFleet(FleetSide.PLAYER, "gsp_pyuma_support", FleetMemberType.SHIP, "Scourge", false);
+    api.addToFleet(FleetSide.PLAYER, "gsp_pyuma_support", FleetMemberType.SHIP, "Six-Pounder", false);
     api.addToFleet(FleetSide.PLAYER, "gsp_okami_assault", FleetMemberType.SHIP, "Grapnel", false);
+    api.addToFleet(FleetSide.PLAYER, "gsp_okami_assault", FleetMemberType.SHIP, "Drunkard", false);
     api.addToFleet(FleetSide.PLAYER, "gsp_kitsune_assault", FleetMemberType.SHIP, "Swashbuckle", false);
     api.addToFleet(FleetSide.PLAYER, "gsp_kitsune_assault", FleetMemberType.SHIP, "Buccaneer", false);
+    api.addToFleet(FleetSide.PLAYER, "gsp_kitsune_assault", FleetMemberType.SHIP, "Cutthroat", false);
 		
 		// The Admiral is Essential
 		api.defeatOnShipLoss("The Bilge Pump");
