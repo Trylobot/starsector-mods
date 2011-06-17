77d5a6c8f4c83084cb33f9a72dadbaf343ab12bb
trylobot <trylobot@c9139e88-106a-4b82-92ae-96bf13407d3a>
2011-06-17 06:11:55 +0000

    git-svn-id: https://trylobot.unfuddle.com/svn/trylobot_starfarer@157 c9139e88-106a-4b82-92ae-96bf13407d3a

--- sc2/data/missions/alliance_v_hierarchy/MissionDefinition.java
+++ sc2/data/missions/alliance_v_hierarchy/MissionDefinition.java
@@ -10,12 +10,6 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 
 	public void defineMission(MissionDefinitionAPI api) {
     
-    // Names --------
-    //   Ur-Quan
-    //     Lord 3, Lord 18, Lord 21, Lord 43, Lord 52, Lord 88, Lord 89, Lord 103, Lord 156, Lord 237, Lord 342, Lord 412, Lord 476, Lord  666, Lord 783, Lord 999
-    //   Earthling
-    //     Adama, Belt, Buck, Decker, Ender, Friedlan, Graeme, Halleck, Kucera, Pike, Pirx, Spiff, Trent, Tuf, VanRijn, Wu
-		
     // Set up the fleets
 		api.initFleet(FleetSide.PLAYER, "", FleetGoal.ATTACK, false);
 		api.initFleet(FleetSide.ENEMY, "", FleetGoal.ATTACK, true);
@@ -25,8 +19,7 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 
     
 		api.addToFleet(FleetSide.PLAYER, "sc2_earthling_cruiser_standard", FleetMemberType.SHIP, "", true);
-		api.addToFleet(FleetSide.PLAYER, "sc2_earthling_cruiser_standard", FleetMemberType.SHIP, "", false);
-		api.addToFleet(FleetSide.PLAYER, "sc2_earthling_cruiser_standard", FleetMemberType.SHIP, "", false);
+		api.addToFleet(FleetSide.PLAYER, "sc2_orz_nemesis_standard", FleetMemberType.SHIP, "", false);
 
 		// Set up the enemy fleet
 		api.addToFleet(FleetSide.ENEMY, "sc2_ur-quan_dreadnought_standard", FleetMemberType.SHIP, "", false);
