563ccee6811ab749dafdbc2b05ab9de9b0a4ff78
trylobot <trylobot@c9139e88-106a-4b82-92ae-96bf13407d3a>
2012-03-08 10:25:23 +0000

    git-svn-id: https://trylobot.unfuddle.com/svn/trylobot_starfarer@263 c9139e88-106a-4b82-92ae-96bf13407d3a

---
+++ nom/data/missions/nomads_demo_2/MissionDefinition.java
@@ -0,0 +1,109 @@
+package data.missions.nomads_demo;
+
+import com.fs.starfarer.api.fleet.FleetGoal;
+import com.fs.starfarer.api.fleet.FleetMemberType;
+import com.fs.starfarer.api.mission.FleetSide;
+import com.fs.starfarer.api.mission.MissionDefinitionAPI;
+import com.fs.starfarer.api.mission.MissionDefinitionPlugin;
+
+public class MissionDefinition implements MissionDefinitionPlugin {
+
+	public void defineMission(MissionDefinitionAPI api) {
+
+		// Set up the fleets
+		api.initFleet(FleetSide.PLAYER, "HSS", FleetGoal.DEFEND, false);
+		api.initFleet(FleetSide.ENEMY, "TTS", FleetGoal.DEFEND, true);
+
+		// Set up the player's fleet
+		api.addToFleet(FleetSide.PLAYER, "nom_gila_monster_antibattleship", FleetMemberType.SHIP, true);
+		api.addToFleet(FleetSide.PLAYER, "nom_gila_monster_antibattleship", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_komodo_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_komodo_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_komodo_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_wurm_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_wurm_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_wurm_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_wurm_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_wurm_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_wurm_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_iguana_assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+
+		// Set up the enemy fleet
+		api.addToFleet(FleetSide.ENEMY, "conquest_Elite", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "conquest_Elite", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "eagle_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "falcon_Attack", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "falcon_Attack", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "venture_Balanced", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "apogee_Balanced", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "dominator_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "dominator_Support", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "condor_Strike", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "condor_FS", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "eagle_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "eagle_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "hammerhead_Elite", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "hammerhead_Balanced", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "hammerhead_Balanced", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "hound_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "hound_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "lasher_CS", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "lasher_CS", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "brawler_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "brawler_Assault", FleetMemberType.SHIP, false);
+		api.addToFleet(FleetSide.ENEMY, "thunder_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.ENEMY, "thunder_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.ENEMY, "talon_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.ENEMY, "talon_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.ENEMY, "talon_wing", FleetMemberType.FIGHTER_WING, false);
+		
+		// Set up the map.
+		float width = 18000f;
+		float height = 14000f;
+		api.initMap((float)-width/2f, (float)width/2f, (float)-height/2f, (float)height/2f);
+		
+		float minX = -width/2;
+		float minY = -height/2;
+		
+		
+		for (int i = 0; i < 50; i++) {
+			float x = (float) Math.random() * width - width/2;
+			float y = (float) Math.random() * height - height/2;
+			float radius = 100f + (float) Math.random() * 400f; 
+			api.addNebula(x, y, radius);
+		}
+		
+		// Add objectives
+		api.addObjective(minX + width * 0.25f, minY + height * 0.25f, "nav_buoy");
+		api.addObjective(minX + width * 0.75f, minY + height * 0.25f, "comm_relay");
+		api.addObjective(minX + width * 0.75f, minY + height * 0.75f, "nav_buoy");
+		api.addObjective(minX + width * 0.25f, minY + height * 0.75f, "comm_relay");
+		api.addObjective(minX + width * 0.5f, minY + height * 0.5f, "sensor_array");
+		
+		// Add a randomized asteroid field, maybe
+		if ((float) Math.random() > 0.5f) {
+			api.addAsteroidField(10f + (float) Math.random() * (width - 20f), 10f + (float) Math.random() * (height - 20f), (float) Math.random() * 360f, (float) Math.random() * 1000 + 1000,
+								 20f, 70f, (int) ((float) Math.random() * 100 + 50));
+		}
+	}
+
+}
+
+
+
+
+
+
