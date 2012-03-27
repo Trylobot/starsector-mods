6ee41834727016f4387c6b5a53388c41f67c681a
trylobot <trylobot@c9139e88-106a-4b82-92ae-96bf13407d3a>
2012-03-27 03:50:28 +0000

    testing & balancing for nomads
    git-svn-id: https://trylobot.unfuddle.com/svn/trylobot_starfarer@289 c9139e88-106a-4b82-92ae-96bf13407d3a

---
+++ nom/data/missions/test1/MissionDefinition.java
@@ -0,0 +1,42 @@
+package data.missions.test1;
+
+import com.fs.starfarer.api.fleet.FleetGoal;
+import com.fs.starfarer.api.fleet.FleetMemberType;
+import com.fs.starfarer.api.mission.FleetSide;
+import com.fs.starfarer.api.mission.MissionDefinitionAPI;
+import com.fs.starfarer.api.mission.MissionDefinitionPlugin;
+
+public class MissionDefinition implements MissionDefinitionPlugin {
+
+	public void defineMission(MissionDefinitionAPI api) {
+
+		// Set up the fleets
+		api.initFleet(FleetSide.PLAYER, "HSS", FleetGoal.DEFEND, false);
+		api.initFleet(FleetSide.ENEMY, "TTS", FleetGoal.DEFEND, true);
+
+		// Set up the player's fleet
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.PLAYER, "nom_scarab_wing", FleetMemberType.FIGHTER_WING, false);
+		// api.addToFleet(FleetSide.PLAYER, "nom_iguana_wing", FleetMemberType.FIGHTER_WING, false);
+
+		// Set up the enemy fleet
+		api.addToFleet(FleetSide.ENEMY, "talon_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.ENEMY, "talon_wing", FleetMemberType.FIGHTER_WING, false);
+		api.addToFleet(FleetSide.ENEMY, "talon_wing", FleetMemberType.FIGHTER_WING, false);
+		//api.addToFleet(FleetSide.ENEMY, "wolf_CS", FleetMemberType.SHIP, false);
+		// api.addToFleet(FleetSide.ENEMY, "broadsword_wing", FleetMemberType.FIGHTER_WING, false);
+
+		// Set up the map.
+		float width = 6500f;
+		float height = 4000f;
+		api.initMap((float)-width/2f, (float)width/2f, (float)-height/2f, (float)height/2f);
+	}
+
+}
+
+
+
+
+
+
