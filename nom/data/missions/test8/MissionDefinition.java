62bd5a6cc9e55d74e8173cc694c9c02b7137a20d
trylobot <trylobot@c9139e88-106a-4b82-92ae-96bf13407d3a>
2012-04-04 02:54:24 +0000

    beginning of yellowjacket
    git-svn-id: https://trylobot.unfuddle.com/svn/trylobot_starfarer@321 c9139e88-106a-4b82-92ae-96bf13407d3a

---
+++ nom/data/missions/test8/MissionDefinition.java
@@ -0,0 +1,35 @@
+package data.missions.test8;
+
+import com.fs.starfarer.api.fleet.FleetGoal;
+import com.fs.starfarer.api.fleet.FleetMemberType;
+import com.fs.starfarer.api.mission.FleetSide;
+import com.fs.starfarer.api.mission.MissionDefinitionAPI;
+import com.fs.starfarer.api.mission.MissionDefinitionPlugin;
+
+public class MissionDefinition implements MissionDefinitionPlugin {
+
+	public void defineMission(MissionDefinitionAPI api) {
+
+		// Set up the fleets
+		api.initFleet(FleetSide.PLAYER, "HSS", FleetGoal.DEFEND, false);
+		api.initFleet(FleetSide.ENEMY, "TTS", FleetGoal.DEFEND, true);
+
+		// Set up the player's fleet
+		api.addToFleet(FleetSide.PLAYER, "nom_yellowjacket_sniper", FleetMemberType.SHIP, true);
+
+		// Set up the enemy fleet
+		api.addToFleet(FleetSide.ENEMY, "lasher_CS", FleetMemberType.SHIP, false);
+
+		// Set up the map.
+		float width = 6500f;
+		float height = 4000f;
+		api.initMap((float)-width/2f, (float)width/2f, (float)-height/2f, (float)height/2f);
+	}
+
+}
+
+
+
+
+
+
