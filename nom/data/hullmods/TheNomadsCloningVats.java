4c601147e6599d03e2f0a42580925fde95559e07
Trylobot <trylobot@gmail.com>
2013-10-02 04:50:21 -0500

    Oasis logistics hullmods specced out

---
+++ nom/data/hullmods/TheNomadsCloningVats.java
@@ -0,0 +1,13 @@
+package data.hullmods;
+import com.fs.starfarer.api.combat.HullModEffect;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import data.hullmods.BaseHullMod;
+
+
+public class TheNomadsCloningVats extends BaseHullMod
+{
+	
+}
