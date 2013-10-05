7c827f0c474d21cdcac6b71719a8f7c7d3f908d5
Trylobot <trylobot@gmail.com>
2013-10-05 15:13:22 -0500

    workaround for exception

--- nom/data/scripts/plugins/TheNomadsCombatEnginePlugin.java
+++ nom/data/scripts/plugins/TheNomadsCombatEnginePlugin.java
@@ -2,19 +2,27 @@ package data.scripts.plugins;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.CombatEnginePlugin;
 import com.fs.starfarer.api.combat.CombatFleetManagerAPI;
+import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
 import com.fs.starfarer.api.fleet.FleetGoal;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.input.InputEventAPI;
 import com.fs.starfarer.api.mission.FleetSide;
 import data.scripts._;
 import java.util.Iterator;
 import java.util.List;
 import org.lwjgl.util.vector.Vector2f;
 
-public class TheNomadsCombatEnginePlugin implements CombatEnginePlugin
+public class TheNomadsCombatEnginePlugin implements CombatEnginePlugin, EveryFrameCombatPlugin
 {
+	private CombatEngineAPI engine = null;
+	private boolean executed = true;
+	
 	public void init( CombatEngineAPI engine )
 	{
-		if( engine.getContext() != null
+		this.engine = engine;
+		executed = false;
+		
+		/*if( engine.getContext() != null
 		&&  engine.getContext().getOtherFleet() != null
 		&&  engine.getContext().getOtherFleet().getFaction() != null
 		&&  "nomads".equals( engine.getContext().getOtherFleet().getFaction().getId() ))
@@ -45,7 +53,48 @@ public class TheNomadsCombatEnginePlugin implements CombatEnginePlugin
 					_.L("NullPointerException in CombatFleetManagerAPI.spawnShipOrWing for "+ship.getSpecId());
 				}
 			}
-		}
+		}*/
+	}
+
+	public void advance( float amount, List events )
+	{
+		if( executed )
+			return;
+		executed = true;
+		if( engine.getContext() != null
+		&&  engine.getContext().getOtherFleet() != null
+		&&  engine.getContext().getOtherFleet().getFaction() != null
+		&&  "nomads".equals( engine.getContext().getOtherFleet().getFaction().getId() ))
+		{
+			CombatFleetManagerAPI fleet_mgr = engine.getFleetManager( FleetSide.ENEMY );
+			List reserves = fleet_mgr.getReservesCopy();
+			for( Iterator i = reserves.iterator(); i.hasNext(); )
+			{
+				FleetMemberAPI ship = (FleetMemberAPI)i.next();
+				if( !ship.canBeDeployedForCombat() )
+					return;
+				
+				Vector2f spawn_location = new Vector2f();
+				if( engine.getContext().getOtherGoal() == FleetGoal.ATTACK )
+				{
+					spawn_location.x = -0.45f * engine.getMapWidth() + 0.90f * (float)Math.random() * engine.getMapWidth();
+					spawn_location.y = 0.50f * engine.getMapHeight() + 0.10f * (float)Math.random() * engine.getMapHeight();
+				}
+				else if( engine.getContext().getOtherGoal() == FleetGoal.ESCAPE )
+				{
+					spawn_location.x = -0.30f * engine.getMapWidth() + 0.60f * (float)Math.random() * engine.getMapWidth();
+					spawn_location.y = -0.20f * engine.getMapHeight() + 0.10f * (float)Math.random() * engine.getMapHeight();
+				}
+				try
+				{
+					fleet_mgr.spawnShipOrWing( ship.getSpecId(), spawn_location, 270.0f );
+				}
+				catch( NullPointerException e )
+				{
+					_.L("NullPointerException in CombatFleetManagerAPI.spawnShipOrWing for "+ship.getSpecId());
+				}
+			}
+		}		
 	}
 
 
